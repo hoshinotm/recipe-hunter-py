2c430174d4803f6a250dda0ef5b13abf350f4ed6
Takayuki <hoshino@acm.org>
2017-10-03 11:37:07 -0500

    Adding user authentication

--- Food2ForkTest.py
+++ Food2ForkTest.py
@@ -11,7 +11,11 @@ import json
 import requests
 from recipe_hunter.RecipeHeaderResponse import RecipeHeaderResponse
 from recipe_hunter.RecipeHeader import RecipeHeader
-
+# from recipe_hunter.GUI import GUI
+# from recipe_hunter.Authentication import Authentication
+from recipe_hunter.LoginWindow import LoginWindow
+from recipe_hunter.User import User
+# from recipe_hunter.ExitCode import ExitCode
 
 DEFAULT_URL = 'http://food2fork.com/api/search'
 DEFAULT_KEYWORD = "sushi"
@@ -23,18 +27,6 @@ DEFAULT_KEYWORD = "sushi"
 class Args(Enum):
     API_KEY_POS = 1
     KEYWORD1_POS = 2
-
-# class CmdLineArgs(object):
-#
-#     def num_of_params(self):
-#         return len(sys.argv)-1
-#
-#     def script_name(self):
-#         return sys.argv[0]
-#
-#     def param_list(self):
-#         if len(sys.argv) > 1:
-#             return sys.argv[1:]
 
 #################
 # get_api_key()
@@ -74,30 +66,37 @@ def get_recipe_header_list():
 # Create a list of recipe header objects loaded from
 # the a response to HTTP get request for recipe headers
 # containing a specified keyword
+#
 def create_header_list(recipe_header_reponse):
+
     header_object_list = list()
     for header_info in recipe_header_response.recipe_header_list:
         header_object_list.append(RecipeHeader(header_info))
     return header_object_list
 
 ###################
+# authenticate_user()
+# Authenticate current user, using the given authentication
+# object
+#
+def authenticate_user(gui, authentication):
+
+    login_window = LoginWindow(gui)
+    login_window.go()
+    return User()
+
+###################
 #
 #  Main
 #
-# payload = {'key': get_api_key(), 'q': get_keywords()[0]}
-# r = requests.get(DEFAULT_URL, params=payload)
 resp_content = get_recipe_header_list().content
 dict = json.loads(resp_content)
-# print(dict)
 recipe_header_response = RecipeHeaderResponse(dict)
-# print( "Count: ", recipe_header_response.num_recipes, "Recipes: ", recipe_header_response.recipe_header_list)
-header_object_list = list()
+# header_object_list = list()
 header_object_list = create_header_list(recipe_header_response)
-print(header_object_list)
-# for header in recipe_header_response.recipe_header_list:
-#     header_object_list.append( RecipeHeader(header))
-
-# print(json.dumps(r.json(), sort_keys=True, indent=4))
+for header in recipe_header_response.recipe_header_list:
+    header_object_list.append( RecipeHeader(header))
+print([header.to_string() for header in header_object_list])
 
 
 
